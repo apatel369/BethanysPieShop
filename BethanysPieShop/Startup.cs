220d8cac801b688fe7354cd88e4aa83b0c7f924a
apatel369 <33442948+apatel369@users.noreply.github.com>
2020-02-22 17:33:06 -0600

    add order form using tag helpers, model binding and validation

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -30,10 +30,10 @@ namespace BethanysPieShop
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
             services.AddHttpContextAccessor();
             services.AddSession();
-
             services.AddControllersWithViews();
         }
 
